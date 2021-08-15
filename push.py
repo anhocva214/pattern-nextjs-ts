6a25b44de107d2c9a99ac4cb71ccf316b787d321
anhocva214 <anhocva214@gmail.com>
2021-08-15 09:12:54 +0700

    update push.py

--- push.py
+++ push.py
@@ -9,5 +9,5 @@ date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 msg = input("Enter message: ")
 
 os.system("git add .")
-os.system("""git commit -m " """+date_time+""" """+msg+""" " """)
+os.system("""git commit -m " """+msg+""" " """)
 os.system("git push origin main")
