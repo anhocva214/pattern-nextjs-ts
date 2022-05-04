271291744436a8db706f962b6dfd8816ec008175
anhocva214 <anhocva214@gmail.com>
2022-05-04 15:13:04 +0700

    update alert redux

--- src/redux/alert.redux.ts
+++ src/redux/alert.redux.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from 'src/redux/reducer'
 import Alert from 'react-s-alert';
+import { AppThunk } from '.';
 
 export interface AlertState {
 
@@ -14,32 +15,38 @@ export const alertSlice = createSlice({
     name: 'alert',
     initialState,
     reducers: {
-        alertSuccess: (state, {payload}: PayloadAction<string>) => {
-            Alert.success(payload, {
-                position: 'top-right',
-                effect: 'slide',
-                timeout: 3000,
-            });
-        },
-        alertError: (state, {payload}: PayloadAction<string>) => {
-            Alert.error(payload, {
-                position: 'top-right',
-                effect: 'slide',
-                timeout: 3000,
-            });
-        },
+       
     },
 })
 
 export const alertReducer = alertSlice.reducer
 export const alertSelector = (state: RootState) => state.alert
+export const actions = alertSlice.actions
 
 
 /*--------------------------*/
 /*------- ACTIONS ----------*/
 /*--------------------------*/
-export const {
-    alertError,
-    alertSuccess
-} = alertSlice.actions
 
+const success = (message: string): AppThunk => async (dispatch) => {
+    Alert.success(message, {
+        position: 'top-right',
+        effect: 'slide',
+        timeout: 3000,
+    });
+}
+
+const error = (message: string): AppThunk => async (dispatch) => {
+    Alert.error(message, {
+        position: 'top-right',
+        effect: 'slide',
+        timeout: 3000,
+    });
+}
+
+
+
+export const alertActions = {
+    success,
+    error
+}
